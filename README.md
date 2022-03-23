456819386ef9b541dc51314775fc1fdc5c56353b
mahuafeng <mahuafeng@ctcn.com.cn>
2022-03-23 16:11:43 +0800

    更细gradle版本

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GitLib 
 ### TagLayout
-![TagLayout效果图](https://github.com/nmssdmf/gitlib/blob/master/pic/TagLayout.png?raw=true "效果图")
+![TagLayout效果图](./pic/TagLayout.jpg "效果图")
 - TagLayout顾名思义是一个显示tag的group,目前支持两种显示方式:规则布局,不规则布局
 - 规则布局:子view的宽高等同,每行显示固定个数
 - 不规则布局:子view的宽高不定
--- gittest/src/main/java/com/nmssdmf/gittest/activity/TagLayoutActivity.java
+++ gittest/src/main/java/com/nmssdmf/gittest/activity/TagLayoutActivity.java
@@ -1,8 +1,8 @@
 package com.nmssdmf.gittest.activity;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.widget.TextView;
 
 import com.nmssdmf.gittest.R;
@@ -14,22 +14,27 @@ public class TagLayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_tag_layout);
+        binding = ActivityTagLayoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         initView();
     }
 
     private void initView() {
-        for (int i = 0; i< 400; i++) {
+        for (int i = 0; i< 50; i++) {
             TextView view = new TextView(this);
             view.setText("Tag " + i);
+            view.setTextSize(14);
+            view.setGravity(Gravity.CENTER);
             view.setBackgroundResource(R.color.colorPrimary);
             binding.tlRule.addView(view);
         }
 
         String string = "abcdefghijklmnopqrstuvwxyz";
-        for (int i =1; i< 25; i++) {
+        for (int i =1; i< string.length(); i++) {
             TextView view = new TextView(this);
             view.setText(string.substring(0, i));
+            view.setTextSize(14);
+            view.setGravity(Gravity.CENTER);
             view.setBackgroundResource(R.color.colorAccent);
             binding.tlIrregular.addView(view);
         }
